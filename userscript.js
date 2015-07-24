ef96d8e7cead720d63deeb88ca65c90ac4f6f29c
lianzhao <knife.fe@gmail.com>
2015-07-24 18:20:12 +0800

    init commit

---
+++ userscript.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         Huiji.Purge
+// @namespace    Huiji
+// @version      0.0.1
+// @description  为灰机添加purge按钮
+// @author       lianzhao
+// @match        http://*.huiji.wiki/*
+// @grant        none
+// ==/UserScript==
+
+$(function(){
+    var current = window.location.href;
+	var index = current.indexOf('/wiki/');
+	console.log('index=' + index);
+	var title;
+	if (index > 0){
+		// normal page
+		title = current.substr(index + '/wiki/'.length);
+		console.log('title=' + title);
+	}
+	else{
+		// not a normal page
+		var index1 = current.indexOf('title=');
+		var index2 = current.indexOf('&');
+		console.log('index1=' + index1);
+		console.log('index2=' + index2);
+		title = current.substr(index1 + 'title='.length, index2 - index1 - 'title='.length);
+		console.log('title=' + title);
+	}
+	var url = '/index.php?title=' + title + '&action=purge';
+	$('.nav-pills').append('<li><a href="' + url + '"><i class="fa fa-refresh"> purge</i></li>');
+});
